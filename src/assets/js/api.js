77e62dc88e37d57bd6a42f1f87cdbb3704de0f4d
kis6905 <kis6905@gmail.com>
2019-07-27 13:23:13 +0900

    Update: 정리

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,10 +1,9 @@
-const postConfig = {
+const getConfig = {
   headers: {
-    'Content-Type': 'application/json;charset=utf-8',
     'Access-Control-Allow-Origin': '*'
   }
 }
-const getConfig = {
+const postConfig = {
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
@@ -14,15 +13,21 @@ const apiDomain = process.env.VUE_APP_API_DOMAIN
 export default {
   methods: {
     async login (data) {
-      const response = await this.$http.post(`${apiDomain}/login`, data, postConfig)
+      const requestConfigForLogin = {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+      const response = await this.$http.post(`${apiDomain}/login`, data, requestConfigForLogin)
       const jwtHeader = response.headers['jwt-header']
-      postConfig.headers['jwt-header'] = jwtHeader
       getConfig.headers['jwt-header'] = jwtHeader
+      postConfig.headers['jwt-header'] = jwtHeader
       return response
     },
     requestGet (url) {
       if (process.env.VUE_APP_MODE === 'L') {
-        getConfig.headers['jwt-header'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IlJPTEVfQURNSU4sUk9MRV9BUFBST1ZBTCxST0xFX1VTRVIiLCJpc3MiOiIxbGVhZiIsImV4cCI6MTU1MTY3NjcxOSwidXNlcklkIjoiaXNrd29uIiwidXNlclNlcSI6MX0.6QVJKAERS2U3j_Yqt-6HIJCgF6BHBilFGkW2URcDaLw'
+        getConfig.headers['jwt-header'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IlJPTEVfQURNSU4sUk9MRV9BUFBST1ZBTCxST0xFX1VTRVIiLCJpc3MiOiIxbGVhZiIsImV4cCI6MTU1NTEyNDMyOSwidXNlcklkIjoiaXNrd29uIiwidXNlclNlcSI6MX0.k-SLA6KCv1gHn9b-fFgzVS2AWiUzIFE0ZUlMOP7oO4M'
       }
       return this.$http.get(`${apiDomain}${url}`, getConfig)
     },
